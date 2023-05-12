34154ce66d16d810ca1b0fd9bfec5c083032733c
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-12 16:30:41 -0700

    added skeleton problem

--- Arrays/217.ContainsDuplicates.js
+++ Arrays/217.ContainsDuplicates.js
@@ -3,3 +3,11 @@
  * return true if any value appears at least twice in the array,
  *  and return false if every element is distinct.
  */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+};
