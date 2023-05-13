a694f1d3b8596ecacbd207fd8dc93099c30c6232
Melrose Ceballos <mcm44@hawaii.edu>
2023-05-12 17:10:11 -0700

    changed starting point of left to i=0

--- Arrays/217.ContainsDuplicates.js
+++ Arrays/217.ContainsDuplicates.js
@@ -10,8 +10,9 @@
  */
 var containsDuplicate = function(nums) {
   if(!(nums.length >= 2)) return false
-   for(let i=1; i<nums.length; i++ ){
+   for(let i=0; i<nums.length; i++ ){
     let left = nums[i];
+    // start right pointer from the 
     for (let j = i + 1; j<nums.length; j++){
         let right = nums[j]
         if(left === right){
@@ -22,4 +23,4 @@ var containsDuplicate = function(nums) {
   return false
 };
 
-console.log(containsDuplicate([1,2,3,4,6,2,9]))
+console.log(containsDuplicate([3,3]))
