0e56aba9aa62ac32bbab87caa6b030babb531d67
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-23 13:21:57 -0700

    Update 121. BestTimeToBuySellStock.js

--- Greedy Algorithms/121. BestTimeToBuySellStock.js
+++ Greedy Algorithms/121. BestTimeToBuySellStock.js
@@ -1,1 +1,113 @@
+// My solution
 
+function prices(arr){
+
+    let left = 0
+
+    let right = 0
+
+    let tempProfit = 0
+
+    let maxProfit = 0
+
+    // check if the array has numbers inside of it
+
+    // or if it has enough number to get max profit
+
+    if(arr.length === 0 ) return 0
+
+    // loop through the array
+
+    for(let i=0; i<arr.length; i++){
+
+        left = arr[i]
+
+        right = arr[i + 1]
+
+        if(right < arr.length){
+
+            tempProfit = right - left
+
+            maxProfit = Math.max(maxProfit, tempProfit)
+
+        }
+
+    }
+
+    return maxProfit + 1
+
+}
+
+console.log(prices ([7,1,5,3,6,4]))
+
+// other solutions
+
+function prices(arr) {
+
+    if (arr.length === 0) return 0;
+
+  
+
+    let minPrice = arr[0];
+
+    let maxProfit = 0;
+
+  
+
+    for (let i = 1; i < arr.length; i++) {
+
+      // Calculate the potential profit if selling on this day
+
+      const potentialProfit = arr[i] - minPrice;
+
+  
+
+      // Update the minimum price if a lower price is found
+
+      minPrice = Math.min(minPrice, arr[i]);
+
+  
+
+      // Update the maximum profit if a higher profit is found
+
+      maxProfit = Math.max(maxProfit, potentialProfit);
+
+    }
+
+  
+
+    return maxProfit;
+
+  }
+
+  
+
+  console.log(prices([7, 1, 5, 3, 6, 4]));
+
+  
+
+  // recreate
+
+  function prices (arr){
+
+    if(arr === 0) return null
+
+    let buy = arr[0]
+
+    let maxProfit = 0
+
+    for (let i=1; i<arr.length; i++){
+
+        tempProfit = arr[i] - buy
+
+        buy = Math.min(arr[i], buy)
+
+        maxProfit = Math.max(tempProfit, maxProfit)
+
+    }
+
+    return maxProfit
+
+  }
+
+  console.log(prices([7, 1, 5, 3, 6, 4]));
