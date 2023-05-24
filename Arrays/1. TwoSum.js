d1a357f62b159a414f2888a9a0b1e8e55311c3d4
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-23 20:01:30 -0700

    Update 1. TwoSum.js

--- Arrays/1. TwoSum.js
+++ Arrays/1. TwoSum.js
@@ -1,29 +1,17 @@
 twoSum = (arr, target) => {
-
     if(arr.length < 2) return false
-
     for(let i=0; i<arr.length; i++) {
-
         let first = arr[i]
-
         for(let j= i + 1; j<arr.length; j++) {
-
             let second = arr[j]
-
             let sum = first + second
-
             if( sum === target){
-
                 return [i, j]
-
             }
-
         }
-
     }
 
     return null
-
 }
 
 console.log(twoSum([2,7,11,15], 9))
