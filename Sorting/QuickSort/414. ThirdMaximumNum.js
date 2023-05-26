4a3cc2c0989d6e5e8eaf1de3d3119d145efc1591
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-26 08:46:05 -0700

    Update 414. ThirdMaximumNum.js

--- Sorting/QuickSort/414. ThirdMaximumNum.js
+++ Sorting/QuickSort/414. ThirdMaximumNum.js
@@ -8,12 +8,17 @@ If the third maximum does not exist, return the maximum number.
 thirdDis = (arr) => {
   let distinctCount = 0;
   let number = [];
+  //check if array length is 2 or less
+ // returns the biggest number
   if (arr.length <= 2) {
     let high = Math.max(...arr)
     return high
   }
 for (let i = 0; i < arr.length; i++) {
     let current = arr[i];
+    // if the current iteration is not in the
+ // new array, then add it in
+
     if (!number.includes(current)) {
       distinctCount++;
       number.push(current);
