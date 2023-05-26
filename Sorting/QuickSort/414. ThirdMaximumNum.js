b47af24790828b5790b7f6193819cb1a3e086aa5
Melrose Ceballos <120954128+melroseceballos@users.noreply.github.com>
2023-05-26 08:09:26 -0700

    Update 414. ThirdMaximumNum.js

--- Sorting/QuickSort/414. ThirdMaximumNum.js
+++ Sorting/QuickSort/414. ThirdMaximumNum.js
@@ -1,1 +1,13 @@
+/** PROBLEM 
 
+
+// MY SOLUTION:
+thirdDis = (arr) => {
+  if (arr.length <= 2) {
+    let high = Math.max(...arr)
+    return high
+  }
+  return null
+}
+
+console.log(thirdDis([5, 2]))
